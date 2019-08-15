2b630d4daea3c3187628b4cfcd177bb740df3f47
mark-renrgyx <53844281+mark-renrgyx@users.noreply.github.com>
2019-08-15 14:41:18 -0400

    completed all requirements

---
+++ IntToString.java
@@ -0,0 +1,12 @@
+package week1.day7.assignment1;
+
+public class IntToString {
+
+	public static void main(String[] args) {
+		int number = 1234;
+		
+		String converted = String.valueOf(number);
+		
+		System.out.println(converted);
+	}
+}
